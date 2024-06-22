96a6262e6b18c1170a23497ec202b3016b0411ec
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-06-22 23:03:35 +0100

    Create Bean

---
+++ src/main/java/coupling/couplingExemple.java
@@ -0,0 +1,8 @@
+package coupling;
+
+public class couplingExemple {
+    public static void main(String[] args){
+        UserManager userManager = new UserManager();
+        System.out.println(userManager.getUserInfo());
+    }
+}
