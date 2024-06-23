154c3a7458a0154fa1dddc117e4771b52104810a
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-06-23 09:31:54 +0100

    create bean

--- src/main/java/bean/App.java
+++ src/main/java/bean/App.java
@@ -1,2 +1,17 @@
-package bean;public class App {
+package bean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            ApplicationContext context = new ClassPathXmlApplicationContext("applicationBeancontext.xml");
+            MyBean myBean = (MyBean) context.getBean("myBean");
+            myBean.showMessage();
+        } catch (Exception e) {
+            System.out.println("Error loading context: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/bean/MyBean.java
+++ src/main/java/bean/MyBean.java
@@ -1,2 +1,20 @@
-package bean;public class MyBean {
+package bean;
+
+public class MyBean {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void showMessage() {
+        System.out.println("msg : "+message);
+    }
+
+    @Override
+    public String toString() {
+        return "MyBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
 }
