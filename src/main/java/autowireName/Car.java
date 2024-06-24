729363f824d14f602f45ceb4a4fd56eb4d411c37
Azer Ben Azzouz <benazzouzazer@gmail.com>
2024-06-24 01:49:34 +0100

    autowired by name (need fix)

---
+++ src/main/java/autowireName/Car.java
@@ -0,0 +1,13 @@
+package autowireName;
+
+public class Car {
+    private Specification specification;
+
+    public Car(Specification specification) {
+        this.specification = specification;
+    }
+
+    public void DisplayDetails(){
+        System.out.println("pecification : " + specification.toString());
+    }
+}
---
+++ src/main/java/autowireName/autowireNameApp.java
@@ -0,0 +1,2 @@
+package autowireName;public class autowireNameApp {
+}
